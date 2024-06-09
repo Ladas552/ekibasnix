acfb7ab7a8ee2f6ee05ae9692e8480470f96d5a5
Ladas552 <l.tokshalov@gmail.com>
2024-06-09 15:01:56 +0500

    tweaked aliesas

--- Home-Manager/tools/fish.nix
+++ Home-Manager/tools/fish.nix
@@ -15,12 +15,16 @@
       clean = "nh clean all";
       yy = "nh os switch -u ~/Nix-dots/NixOS/";
       yyh = "nh home switch -u ~/Nix-dots/Home-Manager/";
-      en = "nvim ~/Nix-dots/NixOS/configuration.nix";
+      en = "nvim ~/Nix-dots/NixOS/";
       eh = "nvim ~/Nix-dots/Home-Manager/home.nix";
       v = "nvim";
       dl-video = "yt-dlp --embed-thumbnail -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4 --output '%(title)s.%(ext)s'";
       dl-clips = "yt-dlp --embed-thumbnail -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4 --ignore-errors --output '/home/ladas552/Videos/clips/%(playlist)s/%(playlist_index)s-%(title)s.%(ext)s' --yes-playlist";
       dl-vocaloid = "yt-dlp --add-metadata --parse-metadata 'playlist_title:%(album)s' --embed-thumbnail --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --output '/home/ladas552/Music/vocaloid/%(playlist_uploader)s/%(playlist)s/%(title)s.%(ext)s' --yes-playlist";
+      mc = "ranger";
+      mcm = "ranger ~/Music/";
+      mcc = "ranger ~/.config/";
+      mcp = "ranger ~/Pictures/";
     };
     shellAliases = {
       cat = ''${pkgs.bat}/bin/bat'';
@@ -28,10 +32,6 @@
       df = ''${pkgs.duf}/bin/duf'';
       copypaste = ''${pkgs.wgetpaste}/bin/wgetpaste'';
       cmatrix = ''${pkgs.unimatrix}/bin/unimatrix -f -s 95'';
-      mc = "${pkgs.ranger}/bin/ranger";
-      mcm = "${pkgs.ranger}/bin/ranger ~/Music/";
-      mcc = "${pkgs.ranger}/bin/ranger ~/.config/";
-      mcp = "${pkgs.ranger}/bin/ranger ~/Pictures/";
     };
   };
 }
